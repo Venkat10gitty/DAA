7944f98331eef242d60cdb2f9de5908f6428ae92
Venkat10gitty <saivenkat10.coc@gmail.com>
2022-04-15 18:14:12 +0530

    first commit

---
+++ FaruuPAT.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+#define MAXL 500
+#define MAXN 100
+
+int isPossible[MAXL][MAXL][MAXN];
+int matrix[MAXN][MAXN];
+char str[MAXN + 1];
+int L;
+
+int go(int start, int end, int need)
+{
+    if (start > end)
+        return 0;
+    if (isPossible[start][end][need] != -1)
+        return isPossible[start][end][need];
+
+    int i, x, y;
+    for (i = start; i < end; i++)
+    {
+        for (x = 0; x < MAXN; x++)
+        {
+            for (y = 0; y < MAXN; y++) if (matrix[x][y] == need)
+            {
+                if (go(start, i, x) == 1 && go(i + 1, end, y) == 1)
+                {
+                    isPossible[start][end][need] = 1;
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    //string str;
+    cin>>str;
+    L = strlen(str);
+    go(0, L - 1, 'a');
+    // while (scanf(" %s", str) == 1)
+    // {
+    //     L = strlen(str);
+    //     memset(isPossible, -1, sizeof(isPossible));
+    //     go(0, L - 1, 'a');
+    // }
+    return 0;
+}
