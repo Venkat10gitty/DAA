3b69383afa33ef312c94501267f3e6d119c3cb82
Venkat10gitty <saivenkat10.coc@gmail.com>
2022-04-21 10:45:13 +0530

    modified file

--- 21apr_prac.cpp
+++ 21apr_prac.cpp
@@ -9,11 +9,11 @@ void printBoard(int board[N][N]) {
    }
 }
 bool isValid(int board[N][N], int row, int col) {
-   for (int i = 0; i < col; i++) //check whether there is queen in the left or not
+   for (int i = 0; i < col; i++) 
       if (board[row][i])
          return false;
    for (int i=row, j=col; i>=0 && j>=0; i--, j--)
-      if (board[i][j]) //check whether there is queen in the left upper diagonal or not
+      if (board[i][j]) 
          return false;
    for (int i=row, j=col; j>=0 && i<N; i++, j--)
       if (board[i][j]) 
