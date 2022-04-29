f4ac9503c4595c41937e75f19aa17e2c50fe8a1a
Venkat10gitty <saivenkat10.coc@gmail.com>
2022-04-29 21:47:52 +0530

    Added Files

---
+++ Nil_LabFat_q2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+ 
+int IncreasingSequence(int arr[], int i, int n, int prev)
+{
+    if (i == n) {
+        return 0;
+    }
+    int excl = IncreasingSequence(arr, i + 1, n, prev);
+    int incl = 0;
+    if (arr[i] > prev) {
+        incl = 1 + IncreasingSequence(arr, i + 1, n, arr[i]);
+    }
+    return max(incl, excl);
+}
+ 
+int main()
+{
+    int size;
+    int arr[size];
+    cin>>size;
+    int k;
+    for(int i=0;i<size;i++) {
+        cin>>k;
+        k=arr[i];
+    }
+    cout<<IncreasingSequence(arr, 0, size, INT_MIN);
+    return 0;
+}
